c57101c412ec0b600c12c0a75123ef6a25781580
12039350 <12039350@student.uts.edu.au>
2015-05-03 21:03:33 +1000

    added files

---
+++ Tutorial/Assets/Scripts/ScreenRelativePosition.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenRelativePosition : MonoBehaviour {
+
+	public enum ScreenEdge {LEFT, RIGHT, TOP, BOTTOM};
+	public ScreenEdge screenEdge;
+	public float yOffset;
+	public float xOffset;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 newPostion = transform.position;
+		Camera camera = Camera.main;
+
+		switch (screenEdge) {
+			case ScreenEdge.RIGHT:
+				newPostion.x = camera.aspect * camera.orthographicSize + xOffset;
+				newPostion.y = yOffset;
+				break;
+
+			case ScreenEdge.TOP:
+				newPostion.y = camera.orthographicSize + yOffset;
+				newPostion.x = xOffset;
+				break;
+
+			case ScreenEdge.BOTTOM:
+				newPostion.y = -camera.orthographicSize + yOffset;
+				newPostion.x = xOffset;
+				break;
+
+			case ScreenEdge.LEFT:
+				newPostion.x = -camera.aspect * camera.orthographicSize + xOffset;
+				newPostion.y = yOffset;
+				break;
+		}
+
+		transform.position = newPostion;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
